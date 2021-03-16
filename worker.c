761196838e863a492fd37c511ced884421a591c3
echoline <echoline@gmail.com>
2021-03-16 14:20:10 -0700

    wheeeeeeeeee *~

--- worker.c
+++ worker.c
@@ -16,7 +16,7 @@ usage(char *argv0)
 }
 
 double**
-loadfile(unsigned long long size, char *filename)
+loadfile(char *filename)
 {
 	double **data;
 	unsigned long long i;
@@ -34,7 +34,7 @@ loadfile(unsigned long long size, char *filename)
 		data[i] = malloc(sizeof(double) * size);
 
 	r = 0;
-	while((buf = Brdline(bio, '\n')) != nil) {
+	while(r < size && (buf = Brdline(bio, '\n')) != nil) {
 		nptr = buf;
 
 		i = 0;
@@ -66,15 +66,11 @@ void
 length(double **mat, int *a)
 {
 	double l;
-	int i, begin;
+	int i;
 
-	l = 0.0;
-
-	begin = a[0];
-
+	l = mat[a[size-1]][a[0]];
 	for (i = 1; i < size; i++)
-		l += mat[begin][a[i]];
-	l += mat[a[size-1]][begin];
+		l += mat[a[i-1]][a[i]];
 
 	if (l < shortest) {
 		shortest = l;
@@ -101,7 +97,7 @@ permute(double **mat, int *a, int l, int r)
 }
 
 void
-ith(int *a, int n, unsigned long long i)
+ith(int *a, unsigned long long n, unsigned long long i)
 {
 	int j, k = 0;
 	unsigned long long *fact = malloc(n * sizeof(unsigned long long));
@@ -140,28 +136,21 @@ main(int argc, char **argv)
 		usage(argv[0]);
 
 	count = 0;
-
 	size = atoll(argv[1]);
-
 	chunksize = atoi(argv[3]);
-
 	start = atoll(argv[4]);
-
 	shortest = 1000000000.0;
 
 	a = malloc(sizeof(int) * size);
-
 	path = malloc(sizeof(int) * size);
 
-	mat = loadfile(size, argv[2]);
+	mat = loadfile(argv[2]);
 
 	ith(a, size, start);
-
 	permute(mat, a, 0, size - 1);
 
-	snprint(buf, 0x400, "%f ", shortest);
-
+	snprint(buf, 0x400 - 1, "%f ", shortest);
 	for (i = 0; i < size; i++)
-		snprint(&buf[strlen(buf)], 0x400 - strlen(buf) - 1, "%d ", path[i]);
+		snprint(&buf[strlen(buf)], 0x400 - strlen(buf) - 1, "%d ", path[i]+1);
 	print("%s", buf);
 }
